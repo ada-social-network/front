a5954f53de3e9bee2bf5b08aee060c1282d1ba84
aliceKatkout <alice.katkout@gmail.com>
2021-11-12 11:02:04 +0100

    add formik, start modal

--- src/components/global/SideBar/SideBar.tsx
+++ src/components/global/SideBar/SideBar.tsx
@@ -1,10 +1,11 @@
-import React, {FunctionComponent} from "react";
+import React, {FunctionComponent, useState} from "react";
 import MenuTitle from "./MenuTitle";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { faCommentDots } from "@fortawesome/free-solid-svg-icons";
 import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
-
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import BdaPostModal from "./BdaPostModal";
 
 
 interface Props {
@@ -12,8 +13,11 @@ interface Props {
 }
 
 const SideBar: FunctionComponent<Props> = ({small}) => {
+
+const [isModalOpen, setIsModalOpen] = useState(false)
   return (
     <>
+
       <div className={small? ("w-14") : ("w-44")}>
         <div className="h-screen flex flex-col bg-white border-4 border-pink overflow-hidden">
           <ul className="flex flex-col py-4">
@@ -37,7 +41,55 @@ const SideBar: FunctionComponent<Props> = ({small}) => {
                 <MenuTitle name={"Promos"} small={small} icon={faUsers}/>
               </a>
             </li>
+            <li>
+              <button onClick={()=> setIsModalOpen(!isModalOpen)}>
+                <MenuTitle name={"Poster un message"} small={small} icon={faEnvelope}/>
+              </button>
+            </li>
           </ul>
+          {isModalOpen ?
+            <div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+              <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+                <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        
+                        <svg className="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                        </svg>
+                      </div>
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                          Deactivate account
+                        </h3>
+                        <div className="mt-2">
+                          <p className="text-sm text-gray-500">
+                            Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                    <button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+                      Deactivate
+                    </button>
+                    <button
+                              type="button"
+                              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                              onClick={() => setIsModalOpen(false)}
+                            >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>  : null }
+     
+            
         </div>
         </div>
     </>
