38980a8afe415d313e39866237b4d532372bc8d9
noemie.leroux <noemie.leroux@manomano.com>
2022-01-28 17:39:32 +0100

    footer v2

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -6,44 +6,13 @@ export default function Footer () {
         {/* eslint-disable-next-line max-len */}
         <div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8'>
           <div className='flex justify-center space-x-6 md:order-2'>
-            {/* <a
-              href='https://linkedin.com/in/trevornjeru'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='text-gray-400 hover:text-gray-500'
-            >
-              <span className='sr-only'>LinkedIn</span>
-              <Icon name='linkedin' />
-            </a> */}
-
-            {/*  <a
-              href='https://github.com/trevv16'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='text-gray-400 hover:text-gray-500'
-            >
-              <span className='sr-only'>GitHub</span>
-              <Icon name='github' />
-            </a> */}
-
             <a
               href='https://adatechschool.fr/'
               target='_blank'
               rel='noopener noreferrer'
               className='text-gray-400 hover:text-gray-500'
-            >
-              <span className='sr-only'>Adahub</span>
-              {/* <Button className='icon' name='web'>  */}
-            </a>
-            {/*  <a
-              href='https://instagram.com/trevva_'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='text-gray-400 hover:text-gray-500'
-            >
-              <span className='sr-only'>Instagram</span>
-              <Icon name='instagram' />
-            </a> */}
+            />
+            <span className='sr-only'>Adahub</span>
           </div>
           <div className='mt-8 md:mt-0 md:order-1'>
             <p className='text-center text-base text-gray-400'>
