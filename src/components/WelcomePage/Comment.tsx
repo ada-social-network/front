b76f7a3f1b0c3610393c6b4193550278a5c5383b
Salma <71652237+Salma116@users.noreply.github.com>
2022-02-11 17:43:50 +0100

    Revert "Salmaandfanny/implement likes comment"

--- src/components/WelcomePage/Comment.tsx
+++ src/components/WelcomePage/Comment.tsx
@@ -1,11 +1,6 @@
 import { FunctionComponent, useEffect, useState } from 'react'
 import DateComponent from './DateComponent'
 import { getUser, User } from '../../services/user.service'
-import { getCommentLikes } from '../../services/post.service'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-import CommentDislikeButton from './CommentDislikeButton'
-import CommentLikeButton from './CommentLikeButton'
 
 interface Props {
   content: string;
@@ -14,52 +9,10 @@ interface Props {
   id: string
 }
 
-export type CommentLike = {
-  id: string;
-  createdAt: Date;
-  updatedAt: Date;
-  deletedAt?: null | Date;
-  userId: string;
-  commentId: string;
-}
-
-export type CommentLikeList = {
-  items: CommentLike[];
-  count: number;
-  isLikedByCurrentUser : boolean;
-}
-
 const Comment: FunctionComponent<Props> = ({ userId, content, createdAt, id }) => {
   const [author, setAuthor] = useState<User|undefined>(undefined)
-  const [likes, setCommentLikes] = useState<CommentLikeList>()
-
-  const newCommentLike = (response : CommentLike, likes : CommentLikeList) => {
-    likes.items.push(response)
-    setCommentLikes({ items: likes.items, count: likes.count + 1, isLikedByCurrentUser: true })
-  }
-
-  const newCommentDislike = (userId : string, likes : CommentLikeList) => {
-    const newItems = likes.items.filter((item) => item.userId !== userId)
-    setCommentLikes({ items: newItems, count: likes.count - 1, isLikedByCurrentUser: false })
-  }
 
   useEffect(() => {
-    getCommentLikes(id)
-      .then((likes) => {
-        setCommentLikes(likes)
-      })
-      .catch(function (error) {
-        if (error.response) {
-          console.log(error.response.data)
-          console.log(error.response.status)
-          console.log(error.response.headers)
-        } else if (error.request) {
-          console.log(error.request)
-        } else {
-          console.log('Error', error.message)
-        }
-        console.log(error.config)
-      })
     getUser(userId).then((response) => {
       setAuthor(response)
     })
@@ -78,29 +31,14 @@ const Comment: FunctionComponent<Props> = ({ userId, content, createdAt, id }) =
 
           <div className="bg-gray-100 flex flex-row dark:bg-gray-700 rounded-xl px-4 pt-2 pb-2.5">
             <div className="font-semibold mr-3 text-sm leading-relaxed">{author?.firstName}</div>
-            <div className="text-normal mx-2 leading-snug md:leading-normal min-w-3/4">
+            <div className="text-normal mx-2 leading-snug md:leading-normal">
               {content}
             </div>
           </div>
-          <div className="flex flex-row text-sm mt-2 my-auto justify-between">
-            <div className ="flex flex-row text-gray-400 text-sm text-left ">
-              {likes?.isLikedByCurrentUser
-                ? <CommentDislikeButton commentId={id} likes={likes} onPost={newCommentDislike} />
-                : <CommentLikeButton commentId={id} likes={likes} onPost={newCommentLike}/>}
-                
-           <p className="mx-1 pb-2">{likes ? likes.count : 'wait ...'}</p>
-           
-          </div>
-
-          <div>
-          <DateComponent date={createdAt} /> 
-          </div>
-
-          </div>
-              
-          </div>
+          <DateComponent date={createdAt} />
         </div>
       </div>
+    </div>
 
   )
 }
