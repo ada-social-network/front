fe388ff61367cec080c6b2ffc3b345f9220fb8f7
manon-coulombe <manon.coulombe@student.adatechschool.fr>
2022-02-11 17:33:56 +0100

    actu bda page

--- src/components/AdminPage/AdminForum/AdminForum.tsx
+++ src/components/AdminPage/AdminForum/AdminForum.tsx
@@ -7,7 +7,7 @@ import CategoryTable from './CategoryTable'
 const AdminPromo: FunctionComponent = () => {
   return (
     <div className="ml-20 mx-4">
-      <h1 className=' my-6'>Gestion du forum</h1>
+      <h1 className='my-6'>Gestion du forum</h1>
       <div className="flex flex-col">
         <PostButton title={'Ajouter une nouvelle Catégorie'} form={<PostCategoryForm/>} onClose={() => { window.location.reload() }} />
         <CategoryTable />
